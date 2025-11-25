e491cbb6c6971125111c8c6f20559f883f6b420d
KJSendrejas <kj.sendrejas@gmail.com>
2025-11-25 15:23:35 +0800

    Updated Inventory Tracking Program

--- Switch_Loop_Arr_Meth.java
+++ Switch_Loop_Arr_Meth.java
@@ -1,60 +1,61 @@
-//Inventory Tracking Program
 import java.util.Scanner;
-public class Switch_Loop_Arr_Meth {
-
-    public static void main(String[] args) {
+public class Switch_Loop_Arr_Meth{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of items to track: ");
-        int numItems = sc.nextInt();
-        String prodname[] = new String[numItems];
-        int quantity[] = new int[numItems];
+        System.out.println("Enter number of products: ");
+        int numprod = sc.nextInt();
 
-        for(int i = 0; i<prodname.length; i++){
-          System.out.println("Enter name of product " + (i+1) + ": ");  
-          prodname[i] = sc.next();
-          System.out.println("Enter quantity of " + prodname[i] + ": ");
-          quantity[i] = sc.nextInt();
+        String products[] = new String[numprod];
+        int quantity[] = new int[numprod];
+
+        for(int i=0; i<products.length; i++){
+            System.out.println("Enter name of product: " + (i+1)+ ": ");
+            products[i] = sc.next();
+            System.out.println("Enter quantity of " + products[i] + ": ");
+            quantity[i] = sc.nextInt();
         }
 
-        displayInventory(prodname, quantity);
-        countTotalQuantity(quantity);
-
-        int countoutofstock=0; //count number of products out of stock
-        for(int i=0; i<prodname.length; i++){
-            if(quantity[i]==0){
-                countoutofstock+=1;
-            }
-        };
-
-        System.out.println("Number of products out of stock: " + countoutofstock);
-
-        int countinstock=0; //count number of products in stock
-        for(int i=0; i<prodname.length;i++){
-            if(quantity[i]>0){
-                countinstock+=1;
-            }
-        }
-
-        System.out.println("Number of products in stock: " + countinstock);
-
-
-        sc.close(); //this will close the scanner object
+        displayAll(products, quantity);
+        System.out.println("\nTotal Quantity: " + countQuantity(quantity));
+        System.out.println("Number of products out-of-stock:" + countOutofStock(quantity));
+        System.out.println("Number of products in-stock:" + countInStock(quantity));
     }
 
-        public static void displayInventory(String[] prodname, int[] quantity) {    //method to display inventory
-            System.out.println("Inventory:");
-            for (int i = 0; i < prodname.length; i++) {
-                System.out.println(prodname[i] + ": " + quantity[i]);
-            }
+    public static void displayAll(String products[], int quantity[]){
+        System.out.println("\nProduct and Quantities");
+        for(int i=0; i<products.length; i++){
+            System.out.println(products[i] + ": " + quantity[i]);
         }
+    }
 
-        public static void countTotalQuantity(int[] quantity){  //method to count total quantity
-            int total=0;
-            for(int i=0; i<quantity.length; i++){
-                total += quantity[i];
-            }
-            System.out.println("Total quantity of all items: " + total);
+    public static int countQuantity(int quantity[]){
+        int total = 0;
+        for(int i=0; i<quantity.length; i++){
+            total+=quantity[i];
         }
-    
-       
+        return total;
+    }
+
+    public static int countOutofStock(int quantity[]){
+        int countOutofStock = 0;
+        for(int i=0; i<quantity.length;i++){
+            if(quantity[i]==0){
+            countOutofStock++; // counOurtofStock +=1;
+            }
+        }
+        return countOutofStock;
+    }
+
+    public static int countInStock(int quantity[]){
+        int countInStock = 0;
+        for(int i=0; i<quantity.length;i++){
+            if(quantity[i]>0){
+            countInStock++; // countInStock +=1;
+            }
+        }
+        return countInStock;
+    }
+
+
+
 }
