bc0e5bcabc98a0432ed02f7ea450fd3ff5ec1b96
KJSendrejas <kj.sendrejas@gmail.com>
2026-01-27 04:52:00 +0800

    Commit

--- GUI_Components.java
+++ GUI_Components.java
@@ -7,26 +7,40 @@ public class GUI_Components extends JFrame {
     }
 
     GUI_Components(){
-        	JLabel lblHeader = new JLabel("Full Name");
-		add(lblHeader).setBounds(25,50,100,30);
+
+		JLabel lblRes = new JLabel("0.00");
+		add(lblRes).setBounds(25,20,100,30);
+
+        JLabel lbl1Num = new JLabel("First Number:");
+		add(lbl1Num).setBounds(25,50,100,30);
 		
-		JTextField txtName = new JTextField("Enter your name");
-		add(txtName).setBounds(25,85,100,30);
+		JTextField txt1Num = new JTextField();
+		add(txt1Num).setBounds(25,80,100,20);
+
+		JLabel lbl2Num = new JLabel("Second Number:");
+		add(lbl2Num).setBounds(25,115,100,30);
 		
-		JPasswordField txtPass = new JPasswordField();
-		add(txtPass).setBounds(25,120,100,30);
+		JTextField txt2Num = new JTextField();
+		add(txt2Num).setBounds(25,145,100,20);
 		
-		JButton btnAdd = new JButton("Submit");
-		add(btnAdd).setBounds(25,155,100,50);
+		// JPasswordField txtPass = new JPasswordField();
+		// add(txtPass).setBounds(25,120,100,30);
 		
-//		btnAdd.setEnabled(false);
-//		txtName.setEditable(false);
-//		txtName.setText("ENTER");
-//		btnAdd.setBackground(Color.CYAN);
-//		lblHeader.setForeground(Color.green);
+		JButton btnAdd = new JButton("Add");
+		add(btnAdd).setBounds(25,175,100,30);
+
+		btnAdd.addActionListener(e-> {
+			int num1 = Integer.parseInt(txt1Num.getText());
+			int num2 = Integer.parseInt(txt2Num.getText());
+			int sum = num1 + num2;
+			lblRes.setText((String.valueOf(sum)));
+		});
+
+		
+		
 		
 		setSize(530,300);
-		setTitle("Sample");
+		setTitle("Basic Calculator");
 		setLayout(null);
 		setUndecorated(false);
 		setResizable(true);
