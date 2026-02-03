6a150bf51ec20eeca628eca75ba9b479b8c6dfac
KJSendrejas <kj.sendrejas@gmail.com>
2026-02-03 11:37:38 +0800

    Jtable and readfile

--- GUI_Enrollment_Add.java
+++ GUI_Enrollment_Add.java
@@ -1,12 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 import javax.swing.JTextField;
+import javax.swing.table.*;
 
 
 public class GUI_Enrollment_Add extends JFrame {
+
+	static DefaultTableModel model;
+	static JTable table;
+	static JScrollPane scrollPane;
+
     public static void main(String[] args) {
         new GUI_Enrollment_Add();
     }
@@ -62,6 +69,15 @@ public class GUI_Enrollment_Add extends JFrame {
 		textField_4.setBounds(48, 354, 188, 27);
 		add(textField_4);
 
+		String[] columns = {"Name", "Address", "Course", "Age", "Last School"};
+		model = new DefaultTableModel(columns, 0);
+		table = new JTable(model);
+		scrollPane = new JScrollPane(table);
+		add(scrollPane).setBounds(250,50,400,300);
+
+		refreshTable();
+
+
         btnNewButton.addActionListener(e-> {
         
             String name = txtName.getText();
@@ -74,6 +90,8 @@ public class GUI_Enrollment_Add extends JFrame {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
+
+			refreshTable();
         });
 
 
@@ -90,7 +108,20 @@ public class GUI_Enrollment_Add extends JFrame {
 
     public static void savetoFile(String name, String address, String course, String age, String school) throws Exception {
         FileWriter fw = new FileWriter("EnrolledStudent.txt", true);
-        fw.write(name + "|" + address + "|" + course + "|" + age + "|" + school + "\n");
+        fw.write(name + "#" + address + "#" + course + "#" + age + "#" + school + "\n");
         fw.close();
     }
+
+	public static void refreshTable(){
+		model.setRowCount(0);
+		try(BufferedReader br = new BufferedReader(new FileReader("EnrolledStudent.txt"))){
+			String line;
+			while((line = br.readLine()) != null){
+				String[] row = line.split("#");
+				model.addRow(row);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 }
